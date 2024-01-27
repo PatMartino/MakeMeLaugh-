4c7e4cab4ee876e99cff6a6a8506b842f57f4094
fatihgorguc@gmail.com <fatihgorguc@gmail.com>
2024-01-27 17:04:22 +0300

    fix

--- Assets/Scripts/GameObjects/TearDrop.cs
+++ Assets/Scripts/GameObjects/TearDrop.cs
@@ -10,23 +10,17 @@ namespace GameObjects
 
         private void OnEnable()
         {
-            ActivatingCollider();
             DestroyTearDrop();
         }
 
         #endregion
 
         #region Private Functions
-
-        private async Task  ActivatingCollider()
-        {
-            await Task.Delay(1500);
-            transform.GetChild(0).GetComponent<Collider2D>().enabled = true;
-        }
+        
 
         private async Task DestroyTearDrop()
         {
-            await Task.Delay(2830);
+            await Task.Delay(1500);
             Destroy(gameObject);
         }
 
